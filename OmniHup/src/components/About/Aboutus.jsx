8c261620fe0c3ce327f38828dc4980fbe8258f8f
ubeydemiiri <Ubeydemiiri@gmail.com>
2025-01-18 23:24:44 +0300

    Create components Aboutus, donante, About-Style

---
+++ OmniHup/src/components/About/Aboutus.jsx
@@ -0,0 +1,43 @@
+
+
+
+function Aboutus() {
+    return (
+        /* start Aboutus*/
+        <section className="about-section">
+        <div className="about-content">
+            <h1 className="about mt-5">About Us</h1>
+            <div className="about-details">
+                <div className="text-section">
+                    <h2>Who we are</h2>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                        labore et
+                        dolore magna aliqua.
+                        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                        consequat.
+                    </p>
+                    <hr />
+                    <h2>What we do</h2>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                        labore et
+                        dolore magna aliqua.
+                        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                        consequat.
+                    </p>
+                    <hr />
+                </div>
+                <div className="image-section">
+                    <img src={Aboutus} alt="About Us Image" />
+                    
+                </div>
+            </div>
+        </div>
+    </section>
+        /*end Aboutus */
+    );
+  };
+  
+  export default Aboutus;
+  
---
+++ OmniHup/src/components/About/Donanteabout.jsx
@@ -0,0 +1,39 @@
+
+
+
+function Donenteabout() {
+    return (
+        /* start donante*/
+        <section className="donate-section">
+        <div className="donate-container">
+            <div className="image-container">
+               
+                <img src="./img/man-preparing-foodbank-poor-peop.jpg" alt="man Image" />
+            </div>
+
+            <div className="text-container">
+                <h2>What We Do For Donate Charity Foundations</h2>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+                    et dolore
+                    magna aliqua enim ad minim veniam.
+                </p>
+
+                <ul className="features-list">
+                    <li>✔ Ut enim ad minim veniam</li>
+                    <li>✔ Consectetur adipiscing elit</li>
+                    <li>✔ Sed do eiusmod tempor incididunt</li>
+                    <li>✔ Ut enim ad minim veniam</li>
+                    <li>✔ Nisi ut aliquip ex ea commodo</li>
+                </ul>
+
+                <button className="donate-btn">Donate Now</button>
+            </div>
+        </div>
+    </section>
+        /* end donante About */
+    );
+  };
+  
+  export default Donenteabout;
+  
